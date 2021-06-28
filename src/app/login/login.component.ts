85bbdf488c46eb86590721686beefc68bf472162
InoaPSilva <inoaliberato@gmail.com>
2021-06-28 18:00:49 -0300

    Connected to back end

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+   form = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });  
+
+  constructor(private fb: FormBuilder,
+              private posted: UserService) {}
 
   ngOnInit(): void {
+  }
+  
+  login(){
+
+    this.posted.postCredentials(this.form.value);
+    console.warn(this.form.value);
+    
   }
 
 }
